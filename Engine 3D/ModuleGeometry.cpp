6f6e528b70d46d5f0391ac25ba31463b59927453
AleixCas95 <leix_05@hotmail.com>
2019-10-02 19:38:03 +0200

    start gldrawelements

--- Engine 3D/ModuleGeometry.cpp
+++ Engine 3D/ModuleGeometry.cpp
@@ -28,24 +28,10 @@ void ModuleGeometry::Draw(){
 
 
 
-	//// Front face
-	//glTexCoord2f(0, 0); glVertex3f(-1, 1, 1); // v1
-	//glTexCoord2f(0, 1); glVertex3f(-1, -1, 1); // v2
-	//glTexCoord2f(1, 1); glVertex3f(1, -1, 1); // v3
-	//glTexCoord2f(0, 0); glVertex3f(-1, 1, 1); // v1
-	//glTexCoord2f(0, 1); glVertex3f(-1, -1, 1); // v2
-	//glTexCoord2f(1, 1); glVertex3f(1, 1, 1); // v0
-	//// Left face
-	//glTexCoord2f(0, 0); glVertex3f(1, 1, 1); // v0
-	//glTexCoord2f(0, 1); glVertex3f(1, -1, 1); // v3
-	//glTexCoord2f(1, 1); glVertex3f(1, -1, -1); // v4
-	//glTexCoord2f(0, 0); glVertex3f(1, 1, 1); // v0
-	//glTexCoord2f(1, 1); glVertex3f(1, -1, -1); // v4
-	//glTexCoord2f(1, 0); glVertex3f(1, 1, -1); // v5
-	//// Back face
+	
 
 	//// Right face
-	GLfloat vertices[] =
+	GLfloat vertex[] =
 
 		//front
 	{ 1.0f,1.0f,0.0f,
@@ -113,21 +99,41 @@ void ModuleGeometry::Draw(){
 
 	};
 
+	//// Front face
+	//glTexCoord2f(0, 0); glVertex3f(-1, 1, 1); // v1
+	//glTexCoord2f(0, 1); glVertex3f(-1, -1, 1); // v2
+	//glTexCoord2f(1, 1); glVertex3f(1, -1, 1); // v3
+	//glTexCoord2f(0, 0); glVertex3f(-1, 1, 1); // v1
+	//glTexCoord2f(0, 1); glVertex3f(-1, -1, 1); // v2
+	//glTexCoord2f(1, 1); glVertex3f(1, 1, 1); // v0
+	//// Left face
+	//glTexCoord2f(0, 0); glVertex3f(1, 1, 1); // v0
+	//glTexCoord2f(0, 1); glVertex3f(1, -1, 1); // v3
+	//glTexCoord2f(1, 1); glVertex3f(1, -1, -1); // v4
+	//glTexCoord2f(0, 0); glVertex3f(1, 1, 1); // v0
+	//glTexCoord2f(1, 1); glVertex3f(1, -1, -1); // v4
+	//glTexCoord2f(1, 0); glVertex3f(1, 1, -1); // v5
+	//// Back face
+
 
 	glEnd();
 
 	glGenBuffers(1, (GLuint*) &(my_id));
 	glBindBuffer(GL_ARRAY_BUFFER, my_id);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 36 * 3, vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 36 * 3, vertex, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, my_indices);
 }
 
 update_status ModuleGeometry::Update(float dt) {
+
 	glEnableClientState(GL_VERTEX_ARRAY);
 	glBindBuffer(GL_ARRAY_BUFFER, my_id);
 	glVertexPointer(3, GL_FLOAT, 0, NULL);
 	// … draw other buffers
 	glDrawArrays(GL_TRIANGLES, 0, 36);
 	glDisableClientState(GL_VERTEX_ARRAY);
+	
+	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, NULL);
 
 	return UPDATE_CONTINUE;
 }
--- Engine 3D/ModuleGeometry.h
+++ Engine 3D/ModuleGeometry.h
@@ -16,5 +16,6 @@ public:
 	void Draw();
 
 	uint my_id = 0;
+	uint my_indices = 0;
 };
 
