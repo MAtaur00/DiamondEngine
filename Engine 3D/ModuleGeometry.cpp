318b2a1b483e12710577eef17980974245ad38d4
AleixCas95 <leix_05@hotmail.com>
2019-10-02 21:20:50 +0200

    Drawelement 2nd cube

--- Engine 3D/ModuleGeometry.cpp
+++ Engine 3D/ModuleGeometry.cpp
@@ -34,94 +34,119 @@ void ModuleGeometry::Draw(){
 	GLfloat vertex[] =
 
 		//front
-	{ 1.0f,1.0f,0.0f,
-		0.0f,1.0f,0.0f,
-		0.0f,0.0f,0.0f,
+	{ 5.0f,1.0f,0.0f,
+		4.0f,1.0f,0.0f,
+		4.0f,0.0f,0.0f,
 
 		//front2
 
-		0.0f, 0.0f, 0.0f,
-		1.0f, 0.0f, 0.0f,
-		1.0f, 1.0f, 0.0f,
+		4.0f, 0.0f, 0.0f,
+		5.0f, 0.0f, 0.0f,
+		5.0f, 1.0f, 0.0f,
 
 		//right
-		1.0f, 0.0f, 0.0f,
-		1.0f, 0.0f, -1.0f,
-		1.0f, 1.0f, -1.0f,
+		4.0f, 0.0f, 0.0f,
+		5.0f, 0.0f, -1.0f,
+		5.0f, 1.0f, -1.0f,
 
 		//right2
-		1.0f, 0.0f, 0.0f,
-		1.0f, 1.0f,-1.0f,
-		1.0f, 1.0f, 0.0f,
+		5.0f, 0.0f, 0.0f,
+		5.0f, 1.0f,-1.0f,
+		5.0f, 1.0f, 0.0f,
 
 		//back
-		1.0f, 0.0f, -1.0f,
-		0.0f, 1.0f, -1.0f,
-		1.0f, 1.0f, -1.0f,
+		5.0f, 0.0f, -1.0f,
+		4.0f, 1.0f, -1.0f,
+		5.0f, 1.0f, -1.0f,
 
 
 		//back2
-		1.0f, 0.0f, -1.0f,
-		0.0f, 0.0f, -1.0f,
-		0.0f, 1.0f, -1.0f,
+		5.0f, 0.0f, -1.0f,
+		4.0f, 0.0f, -1.0f,
+		4.0f, 1.0f, -1.0f,
 
 
 
 		//top
-		0.0f, 1.0f, 0.0f,
-		1.0f, 1.0f, 0.0f,
-		1.0f, 1.0f, -1.0f,
+		4.0f, 1.0f, 0.0f,
+		5.0f, 1.0f, 0.0f,
+		5.0f, 1.0f, -1.0f,
 
 		//top2
-		0.0f, 1.0f, 0.0f,
-		1.0f, 1.0f, -1.0f,
-		0.0f, 1.0f, -1.0f,
+		4.0f, 1.0f, 0.0f,
+		5.0f, 1.0f, -1.0f,
+		4.0f, 1.0f, -1.0f,
 
 		//left
-		0.0f, 0.0f, 0.0f,
-		0.0f, 1.0f, 0.0f,
-		0.0f, 1.0f, -1.0f,
+		4.0f, 0.0f, 0.0f,
+		4.0f, 1.0f, 0.0f,
+		4.0f, 1.0f, -1.0f,
 
 		//left2
-		0.0f, 0.0f, 0.0f,
-		0.0f, 1.0f, -1.0f,
-		0.0f, 0.0f, -1.0f,
+		4.0f, 0.0f, 0.0f,
+		4.0f, 1.0f, -1.0f,
+		4.0f, 0.0f, -1.0f,
 
 		//bottom
-		0.0f, 0.0f, 0.0f,
-		0.0f, 0.0f, -1.0f,
-		1.0f, 0.0f, -1.0f,
+		4.0f, 0.0f, 0.0f,
+		4.0f, 0.0f, -1.0f,
+		5.0f, 0.0f, -1.0f,
 
 		//bottom2
-		0.0f, 0.0f, 0.0f,
-		1.0f, 0.0f, -1.0f,
-		1.0f, 0.0f, 0.0f,
+		4.0f, 0.0f, 0.0f,
+		5.0f, 0.0f, -1.0f,
+		5.0f, 0.0f, 0.0f,
 
 	};
 
-	//// Front face
-	//glTexCoord2f(0, 0); glVertex3f(-1, 1, 1); // v1
-	//glTexCoord2f(0, 1); glVertex3f(-1, -1, 1); // v2
-	//glTexCoord2f(1, 1); glVertex3f(1, -1, 1); // v3
-	//glTexCoord2f(0, 0); glVertex3f(-1, 1, 1); // v1
-	//glTexCoord2f(0, 1); glVertex3f(-1, -1, 1); // v2
-	//glTexCoord2f(1, 1); glVertex3f(1, 1, 1); // v0
-	//// Left face
-	//glTexCoord2f(0, 0); glVertex3f(1, 1, 1); // v0
-	//glTexCoord2f(0, 1); glVertex3f(1, -1, 1); // v3
-	//glTexCoord2f(1, 1); glVertex3f(1, -1, -1); // v4
-	//glTexCoord2f(0, 0); glVertex3f(1, 1, 1); // v0
-	//glTexCoord2f(1, 1); glVertex3f(1, -1, -1); // v4
-	//glTexCoord2f(1, 0); glVertex3f(1, 1, -1); // v5
+	// Front face
+	glTexCoord2f(0, 0); glVertex3f(-1, 1, 1); // v1
+	glTexCoord2f(0, 1); glVertex3f(-1, -1, 1); // v2
+	glTexCoord2f(1, 1); glVertex3f(1, -1, 1); // v3
+	glTexCoord2f(0, 0); glVertex3f(-1, 1, 1); // v1
+	glTexCoord2f(0, 1); glVertex3f(1, -1, 1); // v2
+	glTexCoord2f(1, 1); glVertex3f(1, 1, 1); // v0
+	// Left face
+	glTexCoord2f(1, 1); glVertex3f(1, 1, 1); // v0
+	glTexCoord2f(0, 1); glVertex3f(1, -1, 1); // v3
+	glTexCoord2f(1, 1); glVertex3f(1, -1, -1); // v4
+	glTexCoord2f(0, 0); glVertex3f(1, 1, 1); // v0
+	glTexCoord2f(1, 1); glVertex3f(1, -1, -1); // v4
+	glTexCoord2f(1, 0); glVertex3f(1, 1, -1); // v5
 	//// Back face
-
-
+	glTexCoord2f(0, 0); glVertex3f(-1, -1, -1); // v7
+	glTexCoord2f(1, 1); glVertex3f(-1, 1, -1); // v5
+	glTexCoord2f(0, 1); glVertex3f(1, 1, -1); // v6
+	glTexCoord2f(0, 0); glVertex3f(-1, -1, -1); // v7
+	glTexCoord2f(1, 0); glVertex3f(1, 1, -1); // v4
+	glTexCoord2f(1, 1); glVertex3f(1, -1, -1); // v5
+		// right face
+	glTexCoord2f(0, 0); glVertex3f(-1, -1, 1); //v0
+	glTexCoord2f(0, 1); glVertex3f(-1, 1, 1); // v3
+	glTexCoord2f(1, 0); glVertex3f(-1, 1, -1); // v5
+	glTexCoord2f(0, 1); glVertex3f(-1, -1, 1); // v3
+	glTexCoord2f(1, 1); glVertex3f(-1, 1, -1); // v4
+	glTexCoord2f(1, 0); glVertex3f(-1, -1, -1); // v5
+		//Top face
+	glTexCoord2f(1, 1); glVertex3f(1, 1, 1); // v0
+	glTexCoord2f(1, 0); glVertex3f(1, 1, -1); // v5
+	glTexCoord2f(0, 0); glVertex3f(-1, 1, -1); // v6
+	glTexCoord2f(1, 1); glVertex3f(1, 1, 1); // v0
+	glTexCoord2f(0, 0); glVertex3f(-1, 1, -1); // v6
+	glTexCoord2f(0, 1); glVertex3f(-1, 1, 1); // v1
+		//bottom face
+	glTexCoord2f(1, 0); glVertex3f(1, -1, -1); // v3
+	glTexCoord2f(1, 1); glVertex3f(1, -1, 1); // v4
+	glTexCoord2f(0, 1); glVertex3f(-1, -1, 1); // v7
+	glTexCoord2f(1, 0); glVertex3f(1, -1, -1); // v3
+	glTexCoord2f(0, 1); glVertex3f(-1, -1, 1); // v7
+	glTexCoord2f(0, 0); glVertex3f(-1, -1, -1); // v2
 	glEnd();
 
 	glGenBuffers(1, (GLuint*) &(my_id));
 	glBindBuffer(GL_ARRAY_BUFFER, my_id);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 36 * 3, vertex, GL_STATIC_DRAW);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, my_indices);
+	
 }
 
 update_status ModuleGeometry::Update(float dt) {
@@ -133,7 +158,8 @@ update_status ModuleGeometry::Update(float dt) {
 	glDrawArrays(GL_TRIANGLES, 0, 36);
 	glDisableClientState(GL_VERTEX_ARRAY);
 	
-	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, NULL);
+	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, my_indices);
+	//glDrawElements(GL_TRIANGLES, number_of_indices, GL_UNSIGNED_INT, NULL);
 
 	return UPDATE_CONTINUE;
 }
