bee01193d18c738d804c5986dfef8f9425060e7c
MAtaur00 <mtmtarres@gmail.com>
2019-10-13 19:58:02 +0200

    Mesh* and string added to component Mesh

--- Engine 3D/ComponentMesh.h
+++ Engine 3D/ComponentMesh.h
@@ -1,9 +1,16 @@
 #pragma once
 #include "Component.h"
+#include "Primitive.h"
+#include <string>
+
 class ComponentMesh :
 	public Component
 {
 public:
 	ComponentMesh(GameObject* parent);
 	~ComponentMesh();
+
+public:
+	Mesh* mesh;
+	std::string name;
 };
