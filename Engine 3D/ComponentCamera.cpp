341718b355ec4117687406eca6b4b629cb785b40
MAtaur00 <mtmtarres@gmail.com>
2019-11-12 12:06:41 +0100

    component camera done

--- Engine 3D/ComponentCamera.cpp
+++ Engine 3D/ComponentCamera.cpp
@@ -1,12 +1,65 @@
 #include "ComponentCamera.h"
-
+#include "Application.h"
 
 
 ComponentCamera::ComponentCamera(GameObject* parent) : Component(parent, CompCamera)
 {
+	frustum.type = PerspectiveFrustum;
+
+	frustum.pos = float3::zero;
+	frustum.front = float3::unitZ;
+	frustum.up = float3::unitY;
+
+	frustum.nearPlaneDistance = 1.0f;
+	frustum.farPlaneDistance = 1000.0f;
+	frustum.verticalFov = 60.0f * DEGTORAD;
+	frustum.horizontalFov = 2.f * atan(tan(frustum.verticalFov * 0.5f) * (SCREEN_WIDTH / SCREEN_HEIGHT));
 }
 
 
 ComponentCamera::~ComponentCamera()
 {
+}
+
+void ComponentCamera::Inspector()
+{
+	if (ImGui::CollapsingHeader("Camera", ImGuiTreeNodeFlags_DefaultOpen))
+	{
+		if (frustum.type != InvalidFrustum)
+		{
+			char* camType = frustum.type == PerspectiveFrustum ? "Perspective" : "Orthographic";
+			ImGui::Text("Camera type: ", camType);
+			ImGui::DragFloat("Far plane distance", &frustum.farPlaneDistance, 10, frustum.nearPlaneDistance, 2000.0f);
+			ImGui::DragFloat("Near plane distance", &frustum.nearPlaneDistance, 1, 0.1, frustum.farPlaneDistance);
+
+			float fov = frustum.verticalFov * RADTODEG;
+			if (ImGui::DragFloat("FOV", &fov, 1, 55, 120))
+			{
+				frustum.verticalFov = fov * DEGTORAD;
+				frustum.horizontalFov = 2.f * atan(tan(frustum.verticalFov * 0.5f) * (float(App->window->width) / App->window->height));
+			}
+		}
+		else
+		{
+			ImGui::Text("Invalid camera type");
+		}
+	}
+}
+
+float4x4 ComponentCamera::GetViewMatrix()
+{
+	float4x4 matrix;
+
+	matrix = frustum.ViewMatrix();
+	matrix.Transpose();
+	return matrix;
+}
+
+float4x4 ComponentCamera::GetProjectionMatrix()
+{
+	float4x4 matrix;
+
+	matrix = frustum.ProjectionMatrix();
+	matrix.Transpose();
+	return matrix;
 }
--- Engine 3D/ComponentCamera.h
+++ Engine 3D/ComponentCamera.h
@@ -1,10 +1,20 @@
 #pragma once
 #include "Component.h"
+#include "MathGeoLib/MathGeoLib.h"
+
 class ComponentCamera :
 	public Component
 {
 public:
 	ComponentCamera(GameObject* parent);
 	~ComponentCamera();
-};
 
+	void Inspector();
+
+	float4x4 GetViewMatrix();
+	float4x4 GetProjectionMatrix();
+
+public:
+
+	Frustum frustum;
+};
