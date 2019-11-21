52cfe49713aced8aefdf3702f4deed0abd62bee7
MAtaur00 <mtmtarres@gmail.com>
2019-11-21 13:52:13 +0100

    fixed movement and picking so it only works in editor mode

--- Engine 3D/ModuleCamera3D.cpp
+++ Engine 3D/ModuleCamera3D.cpp
@@ -23,6 +23,8 @@ bool ModuleCamera3D::Start()
 
 	LookAt(float3::zero);
 
+	App->renderer3D->current_cam = compCamera;
+
 	return ret;
 }
 
@@ -43,96 +45,101 @@ update_status ModuleCamera3D::Update()
 	if (!ImGui::IsWindowHovered(ImGuiHoveredFlags_AnyWindow))
 	{
 		float3 newPos(0, 0, 0);
-		float speed = 3.0f * dt;
+		float speed = 3.0f * Time::dt;
 		if (App->input->GetKey(SDL_SCANCODE_LSHIFT) == KEY_REPEAT)
-			speed = 6.0f * dt;
+			speed = 6.0f * Time::dt;
 
 		float3 forward = compCamera->frustum.front * speed;
 		float3 right = compCamera->frustum.WorldRight() * speed;
 
-		if (App->input->GetMouseButton(SDL_BUTTON_RIGHT) == KEY_REPEAT)
+		if (App->renderer3D->current_cam == App->camera->compCamera)
 		{
-			if (App->input->GetKey(SDL_SCANCODE_W) == KEY_REPEAT) newPos += forward;
-			if (App->input->GetKey(SDL_SCANCODE_S) == KEY_REPEAT) newPos -= forward;
 
-			if (App->input->GetKey(SDL_SCANCODE_A) == KEY_REPEAT) newPos -= right;
-			if (App->input->GetKey(SDL_SCANCODE_D) == KEY_REPEAT) newPos += right;
 
-			if (App->input->GetKey(SDL_SCANCODE_Q) == KEY_REPEAT) newPos += float3::unitY * speed;
-			if (App->input->GetKey(SDL_SCANCODE_E) == KEY_REPEAT) newPos -= float3::unitY * speed;
-
-			compCamera->frustum.Translate(newPos);
-		}
-
-		if (App->input->GetKey(SDL_SCANCODE_F) == KEY_REPEAT)
-		{
-			if (App->sceneIntro->current_object)
+			if (App->input->GetMouseButton(SDL_BUTTON_RIGHT) == KEY_REPEAT)
 			{
-				float3 posCam = App->sceneIntro->current_object->transform->GetGlobalPos();
+				if (App->input->GetKey(SDL_SCANCODE_W) == KEY_REPEAT) newPos += forward;
+				if (App->input->GetKey(SDL_SCANCODE_S) == KEY_REPEAT) newPos -= forward;
 
-				compCamera->frustum.pos.x = posCam.x + 10;
-				compCamera->frustum.pos.y = posCam.y + 10;
-				compCamera->frustum.pos.z = posCam.z + 10;
-				LookAt(posCam);
-			}
-		}
+				if (App->input->GetKey(SDL_SCANCODE_A) == KEY_REPEAT) newPos -= right;
+				if (App->input->GetKey(SDL_SCANCODE_D) == KEY_REPEAT) newPos += right;
 
-		if (App->input->GetMouseZ() != 0)
-		{
-			compCamera->frustum.pos += compCamera->frustum.front * cameraSpeed * App->input->GetMouseZ();
-		}
+				if (App->input->GetKey(SDL_SCANCODE_Q) == KEY_REPEAT) newPos += float3::unitY * speed;
+				if (App->input->GetKey(SDL_SCANCODE_E) == KEY_REPEAT) newPos -= float3::unitY * speed;
 
-		if (App->input->GetMouseButton(SDL_BUTTON_LEFT) == KEY_REPEAT &&
-			App->input->GetKey(SDL_SCANCODE_LALT) == KEY_REPEAT)
-		{
-			float Sensitivity = 0.1f;
-			int dx = -App->input->GetMouseXMotion() * Sensitivity;
-			int dy = -App->input->GetMouseYMotion() * Sensitivity;
-
-			if (App->sceneIntro->current_object) {
-				float3 object = App->sceneIntro->current_object->transform->GetGlobalPos();
-
-				math::Quat rotationX = math::Quat::RotateAxisAngle({ 0.0f,1.0f,0.0f }, -App->input->GetMouseXMotion() * Sensitivity * DEGTORAD);
-				math::Quat rotationY = math::Quat::RotateAxisAngle(compCamera->frustum.WorldRight(), -App->input->GetMouseYMotion() * Sensitivity * DEGTORAD);
-				math::Quat finalRotation = rotationX * rotationY;
-
-				compCamera->frustum.up = finalRotation * compCamera->frustum.up;
-				compCamera->frustum.front = finalRotation * compCamera->frustum.front;
-
-				float distance = (compCamera->frustum.pos - object).Length();
-				compCamera->frustum.pos = object + (-compCamera->frustum.front * distance);
-			}
-		}
-
-		// Mouse motion ----------------
-
-		else if (App->input->GetMouseButton(SDL_BUTTON_RIGHT) == KEY_REPEAT)
-		{
-			float Sensitivity = 0.001f;
-			int dx = -App->input->GetMouseXMotion();
-			int dy = -App->input->GetMouseYMotion();		
-
-			if (dx != 0)
-			{
-				float DeltaX = (float)dx * Sensitivity;
-
-				Quat rotation = Quat::RotateY(DeltaX);
-				compCamera->frustum.front = rotation.Mul(compCamera->frustum.front).Normalized();
-				compCamera->frustum.up = rotation.Mul(compCamera->frustum.up).Normalized();
+				compCamera->frustum.Translate(newPos);
 			}
 
-			if (dy != 0)
+			if (App->input->GetKey(SDL_SCANCODE_F) == KEY_REPEAT)
 			{
-				float DeltaY = (float)dy * Sensitivity;
-
-				Quat rotation = Quat::RotateAxisAngle(compCamera->frustum.WorldRight(), DeltaY);
-
-				float3 futureFrustumUp = rotation.Mul(compCamera->frustum.up).Normalized();
-
-				if (futureFrustumUp.y > 0.0f)
+				if (App->sceneIntro->current_object)
 				{
+					float3 posCam = App->sceneIntro->current_object->transform->GetGlobalPos();
+
+					compCamera->frustum.pos.x = posCam.x + 10;
+					compCamera->frustum.pos.y = posCam.y + 10;
+					compCamera->frustum.pos.z = posCam.z + 10;
+					LookAt(posCam);
+				}
+			}
+
+			if (App->input->GetMouseZ() != 0)
+			{
+				compCamera->frustum.pos += compCamera->frustum.front * cameraSpeed * App->input->GetMouseZ();
+			}
+
+			if (App->input->GetMouseButton(SDL_BUTTON_LEFT) == KEY_REPEAT &&
+				App->input->GetKey(SDL_SCANCODE_LALT) == KEY_REPEAT)
+			{
+				float Sensitivity = 0.1f;
+				int dx = -App->input->GetMouseXMotion() * Sensitivity;
+				int dy = -App->input->GetMouseYMotion() * Sensitivity;
+
+				if (App->sceneIntro->current_object) {
+					float3 object = App->sceneIntro->current_object->transform->GetGlobalPos();
+
+					math::Quat rotationX = math::Quat::RotateAxisAngle({ 0.0f,1.0f,0.0f }, -App->input->GetMouseXMotion() * Sensitivity * DEGTORAD);
+					math::Quat rotationY = math::Quat::RotateAxisAngle(compCamera->frustum.WorldRight(), -App->input->GetMouseYMotion() * Sensitivity * DEGTORAD);
+					math::Quat finalRotation = rotationX * rotationY;
+
+					compCamera->frustum.up = finalRotation * compCamera->frustum.up;
+					compCamera->frustum.front = finalRotation * compCamera->frustum.front;
+
+					float distance = (compCamera->frustum.pos - object).Length();
+					compCamera->frustum.pos = object + (-compCamera->frustum.front * distance);
+				}
+			}
+
+			// Mouse motion ----------------
+
+			else if (App->input->GetMouseButton(SDL_BUTTON_RIGHT) == KEY_REPEAT)
+			{
+				float Sensitivity = 0.001f;
+				int dx = -App->input->GetMouseXMotion();
+				int dy = -App->input->GetMouseYMotion();
+
+				if (dx != 0)
+				{
+					float DeltaX = (float)dx * Sensitivity;
+
+					Quat rotation = Quat::RotateY(DeltaX);
 					compCamera->frustum.front = rotation.Mul(compCamera->frustum.front).Normalized();
-					compCamera->frustum.up = futureFrustumUp;
+					compCamera->frustum.up = rotation.Mul(compCamera->frustum.up).Normalized();
+				}
+
+				if (dy != 0)
+				{
+					float DeltaY = (float)dy * Sensitivity;
+
+					Quat rotation = Quat::RotateAxisAngle(compCamera->frustum.WorldRight(), DeltaY);
+
+					float3 futureFrustumUp = rotation.Mul(compCamera->frustum.up).Normalized();
+
+					if (futureFrustumUp.y > 0.0f)
+					{
+						compCamera->frustum.front = rotation.Mul(compCamera->frustum.front).Normalized();
+						compCamera->frustum.up = futureFrustumUp;
+					}
 				}
 			}
 		}
--- Engine 3D/ModulePicking.cpp
+++ Engine 3D/ModulePicking.cpp
@@ -16,71 +16,74 @@ update_status ModulePicking::Update()
 {
 	if (!ImGui::IsWindowHovered(ImGuiHoveredFlags_AnyWindow))
 	{
-		if (App->input->GetMouseButton(SDL_BUTTON_MIDDLE) == KEY_DOWN && App->input->GetKey(SDL_SCANCODE_LALT) != KEY_REPEAT)
+		if (App->renderer3D->current_cam == App->camera->compCamera)
 		{
-			float mouseX = -(1.f - ((float(App->input->GetMouseX()) * 2.f) / (float)App->window->width));
-			float mouseY = (1.f - ((float(App->input->GetMouseY()) * 2.f) / (float)App->window->height));
-
-			LineSegment picking = App->camera->compCamera->frustum.UnProjectLineSegment(mouseX, mouseY);
-
-			float smallerDist = -1.0f;
-
-			GameObject* closestObject = nullptr;
-
-			std::list<GameObject*> candidates;
-
-			for (std::list<GameObject*>::iterator it = App->game_object->gameObjects.begin(); it != App->game_object->gameObjects.end(); ++it)
+			if (App->input->GetMouseButton(SDL_BUTTON_MIDDLE) == KEY_DOWN && App->input->GetKey(SDL_SCANCODE_LALT) != KEY_REPEAT)
 			{
-				if ((*it)->active && (*it)->HasComponent(Object_Type::CompMesh))
-				{
-					LineSegment ray(picking);
-					/*ray.Transform((*it)->transform->GetMatrix().Inverted());*/
+				float mouseX = -(1.f - ((float(App->input->GetMouseX()) * 2.f) / (float)App->window->width));
+				float mouseY = (1.f - ((float(App->input->GetMouseY()) * 2.f) / (float)App->window->height));
 
-					if (ray.Intersects((*it)->boundingBox))
+				LineSegment picking = App->camera->compCamera->frustum.UnProjectLineSegment(mouseX, mouseY);
+
+				float smallerDist = -1.0f;
+
+				GameObject* closestObject = nullptr;
+
+				std::list<GameObject*> candidates;
+
+				for (std::list<GameObject*>::iterator it = App->game_object->gameObjects.begin(); it != App->game_object->gameObjects.end(); ++it)
+				{
+					if ((*it)->active && (*it)->HasComponent(Object_Type::CompMesh))
 					{
-						candidates.push_back(*it);
+						LineSegment ray(picking);
+						/*ray.Transform((*it)->transform->GetMatrix().Inverted());*/
+
+						if (ray.Intersects((*it)->boundingBox))
+						{
+							candidates.push_back(*it);
+						}
 					}
 				}
-			}
 
-			for (std::list<GameObject*>::iterator it = candidates.begin(); it != candidates.end(); ++it)
-			{
-				LineSegment ray(picking);
-				ray.Transform((*it)->transform->GetMatrix().Inverted());
-
-				ComponentMesh* mesh = (ComponentMesh*)(*it)->GetComponent(Object_Type::CompMesh);
-				if (mesh)
+				for (std::list<GameObject*>::iterator it = candidates.begin(); it != candidates.end(); ++it)
 				{
-					Triangle triangle;
-					for (int i = 0; i < mesh->mesh->index.size / 3; ++i)
+					LineSegment ray(picking);
+					ray.Transform((*it)->transform->GetMatrix().Inverted());
+
+					ComponentMesh* mesh = (ComponentMesh*)(*it)->GetComponent(Object_Type::CompMesh);
+					if (mesh)
 					{
-						triangle.a.x = mesh->mesh->vertex.data[mesh->mesh->index.data[i * 3] * 3];
-						triangle.a.y = mesh->mesh->vertex.data[mesh->mesh->index.data[i * 3] * 3 + 1];
-						triangle.a.z = mesh->mesh->vertex.data[mesh->mesh->index.data[i * 3] * 3 + 2];
-
-						triangle.b.x = mesh->mesh->vertex.data[mesh->mesh->index.data[i * 3 + 1] * 3];
-						triangle.b.y = mesh->mesh->vertex.data[mesh->mesh->index.data[i * 3 + 1] * 3 + 1];
-						triangle.b.z = mesh->mesh->vertex.data[mesh->mesh->index.data[i * 3 + 1] * 3 + 2];
-
-						triangle.c.x = mesh->mesh->vertex.data[mesh->mesh->index.data[i * 3 + 2] * 3];
-						triangle.c.y = mesh->mesh->vertex.data[mesh->mesh->index.data[i * 3 + 2] * 3 + 1];
-						triangle.c.z = mesh->mesh->vertex.data[mesh->mesh->index.data[i * 3 + 2] * 3 + 2];
-
-						float distance;
-						float3 position;
-
-						if (ray.Intersects(triangle, &distance, &position))
+						Triangle triangle;
+						for (int i = 0; i < mesh->mesh->index.size / 3; ++i)
 						{
-							if (distance < smallerDist || smallerDist == -1.0f)
+							triangle.a.x = mesh->mesh->vertex.data[mesh->mesh->index.data[i * 3] * 3];
+							triangle.a.y = mesh->mesh->vertex.data[mesh->mesh->index.data[i * 3] * 3 + 1];
+							triangle.a.z = mesh->mesh->vertex.data[mesh->mesh->index.data[i * 3] * 3 + 2];
+
+							triangle.b.x = mesh->mesh->vertex.data[mesh->mesh->index.data[i * 3 + 1] * 3];
+							triangle.b.y = mesh->mesh->vertex.data[mesh->mesh->index.data[i * 3 + 1] * 3 + 1];
+							triangle.b.z = mesh->mesh->vertex.data[mesh->mesh->index.data[i * 3 + 1] * 3 + 2];
+
+							triangle.c.x = mesh->mesh->vertex.data[mesh->mesh->index.data[i * 3 + 2] * 3];
+							triangle.c.y = mesh->mesh->vertex.data[mesh->mesh->index.data[i * 3 + 2] * 3 + 1];
+							triangle.c.z = mesh->mesh->vertex.data[mesh->mesh->index.data[i * 3 + 2] * 3 + 2];
+
+							float distance;
+							float3 position;
+
+							if (ray.Intersects(triangle, &distance, &position))
 							{
-								smallerDist = distance;
-								closestObject = (*it);
+								if (distance < smallerDist || smallerDist == -1.0f)
+								{
+									smallerDist = distance;
+									closestObject = (*it);
+								}
 							}
 						}
 					}
 				}
+				App->sceneIntro->current_object = closestObject;
 			}
-			App->sceneIntro->current_object = closestObject;
 		}
 	}
 	return UPDATE_CONTINUE;
