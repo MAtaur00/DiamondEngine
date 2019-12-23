988bfad246e89938d3723b35f9cc78778d1cf278
MAtaur00 <mtmtarres@gmail.com>
2019-12-23 19:02:22 +0100

    added plane to manager

--- Engine 3D/ModuleParticleManager.cpp
+++ Engine 3D/ModuleParticleManager.cpp
@@ -11,45 +11,50 @@ ModuleParticleManager::~ModuleParticleManager()
 {
 }
 
+bool ModuleParticleManager::Start()
+{
+	plane = new ParticlePlane();
+
+	return true;
+}
+
 int ModuleParticleManager::GetLastParticle()
 {
 	for (int i = lastUsedParticle; i < MAX_PARTICLES; i++) {
-		if (particles[i].life < 0) {
+		if (!particles[i].active) {
 			lastUsedParticle = i;
 			return i;
 		}
 	}
 
 	for (int i = 0; i < lastUsedParticle; i++) {
-		if (particles[i].life < 0) {
+		if (!particles[i].active) {
 			lastUsedParticle = i;
 			return i;
 		}
 	}
-
 	return 0;
 }
 
 update_status ModuleParticleManager::Update()
 {
-
-
 	for (std::list<ComponentEmitter*>::iterator iterator = emitters.begin(); iterator != emitters.end(); ++iterator)
 	{
-
 		(*iterator)->Update();
-
 	}
 
-	return update_status();
+	Draw();
+
+	return UPDATE_CONTINUE;
 }
 
 void ModuleParticleManager::Draw()
 {
-
-
-
-
+	for (int i = 0; i < MAX_PARTICLES; ++i)
+	{
+		if (particles[i].active)
+		{
+			particles[i].Draw();
+		}
+	}
 }
-
-
--- Engine 3D/ModuleParticleManager.h
+++ Engine 3D/ModuleParticleManager.h
@@ -3,6 +3,7 @@
 #include "ComponentEmitter.h"
 #include "Particle.h"
 #include <list>
+#include "ParticlePlane.h"
 
 #define MAX_PARTICLES 10000
 
@@ -11,6 +12,8 @@ class ModuleParticleManager : public Module
 public:
 	ModuleParticleManager(Application* app, bool start_enabled = true);
 	~ModuleParticleManager();
+
+	bool Start();
 
 	int GetLastParticle();
 
@@ -25,4 +28,5 @@ public:
 	int lastUsedParticle = 0;
 	int activeParticles = 0;
 	
+	ParticlePlane* plane = nullptr;
 };
